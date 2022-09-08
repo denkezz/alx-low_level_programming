a3d9694c245a7f553b61b57aefbdad49a87d6557
daniel <denkezzy@gmail.com>
2022-09-08 14:23:15 -0700

    corrected task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main-first print
- *
- * return: always 0 (success)
- */
-
-int main(void)
-{                     
-       puts("\"Programming is like building a multilingual puzzle");
-       Return(0);
+ * main - Entry point
+ * Description: "This program is outputting a string"
+ * Return: Always 0 (Success)
+ */                                                                                                                                int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
