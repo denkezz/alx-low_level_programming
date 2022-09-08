69934aab6b1c3a85644dac7094bda0475ea7ddf7
daniel <denkezzy@gmail.com>
2022-09-08 14:53:30 -0700

    corrected task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 /**
  * main - Entry point
  * Description: "This program is outputting a string"
- * Return: Always 0 (Success)
- */                                                                                                                                int main(void)
+ * Return: Always 0 (Success) 
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
