911fb020b978d0a656202100dbf4ae60fb5cb84c
daniel <denkezzy@gmail.com>
2022-09-10 10:33:21 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints hexadecimal
+ * Return: always 0(success)
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
