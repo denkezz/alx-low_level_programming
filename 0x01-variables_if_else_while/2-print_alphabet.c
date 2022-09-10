85e40e24a5da0e5939314987d091c00512bc9289
daniel <denkezzy@gmail.com>
2022-09-10 07:53:42 -0700

    corrected task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * Description: programming is positive, zero, or negative
+ * Return: always 0(succes)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ;
