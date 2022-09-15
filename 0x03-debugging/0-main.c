1d38e590481e15545177ec14b134dc3e3fcf81c0
daniel <denkezzy@gmail.com>
2022-09-15 05:22:37 -0700

    task 1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - tests funtion that if integer is positive or negative
+ * Return: always 0(success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
